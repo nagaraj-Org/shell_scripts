9fbcb87d1ea1ea37f68c47007dfb9413e73ecd18
nagaraj602 <nagarajkamath602@outlook.com>
2026-03-08 15:28:57 +0000

    Removed files from main

--- arithmetic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-num1=$1
-num2=$2
-
-
-sub=`expr $num1 - $num2`
-
-echo "The difference 0f 2 numbers is $sub"
--- reverse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Agruments in reverse order"
-
-for (( i=$#; i>=1; i-- ))
-do
-	echo "${!i}" 
-done
